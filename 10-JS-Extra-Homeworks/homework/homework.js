ab62fb4da747ba8d6ef78f80bff33b18fcfafca4
selenegabriela <sel_nn@hotmail.com>
2021-09-08 23:23:37 -0500

    last homeworks

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,11 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  let arr = [];
+  for(let propiedad in objeto){
+    arr.push([propiedad, objeto[propiedad]]);
+  }
+  return arr;
 }
 
 
@@ -18,6 +23,27 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  let newString = '';
+  string = string.toLowerCase();
+  for(let i = 0; i < string.length; i++){
+    if(string[i] !== ' '){
+     newString += string[i];
+    }
+  }
+  string = newString;
+
+  let valorActual;
+  let obj = {};
+  let contador = 0;
+
+  for(let i = 0; i < string.length; i++){
+    valorActual = string[i];
+    if(!obj[valorActual]){
+      obj[valorActual] = 0;
+    }
+    obj[valorActual]++;  
+  }
+  return obj;
 }
 
 
@@ -26,6 +52,17 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  let mayusculas = '';
+  let minusculas = '';
+  for(let i = 0; i < s.length; i++){
+    if(s[i] === s[i].toUpperCase()){
+      mayusculas += s[i];
+    } else {
+      minusculas += s[i];
+    }
+  }
+  s = mayusculas + minusculas;
+  return s;
 }
 
 
@@ -35,6 +72,31 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  let reverse = '';
+  let arr = [];
+  let word = '';
+  let mirrorString = '';
+  let contador = -1;
+  for(let i = str.length -1; i >= 0; i--){ // rorrim a sA
+    reverse += str[i];
+  }
+  
+  for(let j = 0; j < reverse.length; j++){ // arr = ['rorrim', 'a', 'sA']
+    if(reverse[j] !== ' '){
+      word += reverse[j];
+    } else {
+      contador++;
+      arr[contador]= word;
+      word = '';
+    } 
+  }
+
+  for(let k = arr.length - 1; k >= 0; k--){ // 'a rorrim'
+    mirrorString += ' ' + arr[k];
+  }
+
+  let finalString = word + mirrorString; // 'sA a rorrim'
+  return finalString;
 } 
 
 
@@ -43,6 +105,17 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  numero = numero.toString(); //'1258521'
+  let reverse = '';
+  for(let i = numero.length -1; i >= 0; i--){
+    reverse += numero[i];
+  }
+
+  if(numero === reverse){
+    return 'Es capicua';
+  } else {
+    return 'No es capicua';
+  }
 }
 
 
@@ -50,6 +123,13 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  let nuevaCadena = '';
+  for(let i = 0; i < cadena.length; i++){
+    if(cadena[i] !== 'a' && cadena[i] !== 'b' && cadena[i] !== 'c'){
+      nuevaCadena += cadena[i];
+    }
+  }
+  return nuevaCadena;
 }
 
 
@@ -57,6 +137,16 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  let provisional;
+  for(let i = 0; i < arr.length; i++)
+    for(let j = 1; j < arr.length; j++){ //
+        if(arr[j-1].length > arr[j].length){
+          provisional = arr[j-1];
+          arr[j-1] = arr[j];
+          arr[j] = provisional;
+      }
+  }
+  return arr;
 }
 
 
@@ -65,7 +155,16 @@ function buscoInterseccion(arreglo1, arreglo2){
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
+  //Escribe tu código aquí 
+  let arregloFinal = [];
+  for(let i = 0; i < arreglo1.length; i++){
+    for(let j = 0; j < arreglo2.length; j++){
+      if(arreglo1[i] === arreglo2[j]){
+        arregloFinal.push(arreglo1[i]);
+      }
+    }
+  }
+  return arregloFinal;
 }
 
 
